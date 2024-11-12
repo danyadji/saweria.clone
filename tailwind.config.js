5043cb52f01cb40469d92386422f287e02c99a1d
danyadji <danyadji90@gmail.com>
2024-11-12 14:13:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"], // Sesuaikan jika ada path lain
+  theme: {
+    extend: {
+      colors: {
+        button: '#FAAE2B',
+        login: '#8BD3DD',
+        biru1 :'#F2F7F5',
+        pink:'#FFBDC4',
+      },
+      fontFamily: {
+          sans: ['Courier New', 'monospace','Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
